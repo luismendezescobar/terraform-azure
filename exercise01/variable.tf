1e834e32dc75134715b5650276738aa6efe1ddd4
esf25t3 <luis.mendez@atos.net>
2021-08-18 18:02:47 -0500

    adding files 8182021-15

--- exercise01/variable.tf
+++ exercise01/variable.tf
@@ -1,9 +1,9 @@
 variable "region"{
-    default="westus"                     #this has to be same region as resource group
+    default="centralus"                     #this has to be same region as resource group
     description="Azure Region"
 }
 variable "ResourceGroup"{
-    default="185-7f98aa58-create-azure-nsgs-with-terraform-8w8"
+    default="185-9b239f77-create-azure-nsgs-with-terraform-8w8"
     description="Here goes the resource group for the storage account. copy the default from the play ground"
 }
 
