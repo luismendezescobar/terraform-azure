f258f246bfa2c32d8b99869933ab288559527ae1
esf25t3 <luis.mendez@atos.net>
2021-08-18 11:17:29 -0500

    adding files 8182021-01

--- exercise01/main.tf
+++ exercise01/main.tf
@@ -1,11 +1,11 @@
 provider "azurerm" {   
-    features{}
+    #features{}
           
 }
  #version=2.72       
 terraform{
     backend "azurerm"{
-        resource_group_name ="1-1b461b3a-playground-sandbox"   #the resource group name needs to be copied from the playground as it's not possible to create resource groups
+        resource_group_name ="183-1c80b169-deploy-an-azure-file-share-with-terra"   #the resource group name needs to be copied from the playground as it's not possible to create resource groups
         storage_account_name="storage4terra8172021"                #you have to made up this name everytime as it needs to be different, I suggest the date of the day
         container_name      ="statefile"
         key                 ="terraform.tfstate"
@@ -28,6 +28,7 @@ terraform{
 #}
 
 #with this one we are going to create a storage account
+/*
 resource "azurerm_storage_account" "sa"{
     name=var.Storage_Account_name
     resource_group_name=var.ResourceGroup
@@ -38,4 +39,31 @@ resource "azurerm_storage_account" "sa"{
         environment="Terraform Storage"
         CreatedBy= "Luis Mendez"
     }
+}
+*/
+
+resource "azurerm_storage_account" "lab" {
+  name                     = "mystorage8182021"
+  resource_group_name      = "183-1c80b169-deploy-an-azure-file-share-with-terra"
+  location                 = var.region
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "lab" {
+  name                  = "blobcontainer4lab"
+  storage_account_name  = azurerm_storage_account.lab.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "lab" {
+  name                   = "TerraformBlob"
+  storage_account_name   = azurerm_storage_account.lab.name
+  storage_container_name = azurerm_storage_container.lab.name
+  type                   = "Block"
+}
+resource "azurerm_storage_share" "lab" {
+  name                 = "terraformshare"  
+  storage_account_name = azurerm_storage_account.lab.name
+  quota                = 50
 }
