1935d28c436366429ccf1f5ee68b8a66441a24f1
esf25t3 <luis.mendez@atos.net>
2021-08-18 17:27:16 -0500

    adding files 8182021-09

--- exercise01/main.tf
+++ exercise01/main.tf
@@ -71,3 +71,30 @@ resource "azurerm_storage_share" "lab" {
   storage_account_name = azurerm_storage_account.lab.name
   quota                = 50
 }
+
+####################################################################################################################
+# Create virtual network
+resource "azurerm_virtual_network" "TFNet" {
+    name                = "network8182021"
+    address_space       = ["10.0.0.0/16"]
+    location            = var.region
+    resource_group_name = var.ResourceGroup
+
+    tags = {
+        environment = "Terraform Networking"
+    }
+}
+
+# Create subnet
+resource "azurerm_subnet" "tfsubnet" {
+    name                 = "LabSubnet"
+    resource_group_name = var.ResourceGroup
+    virtual_network_name = azurerm_virtual_network.TFNet.name
+    address_prefix       = "10.0.1.0/24"
+}
+resource "azurerm_subnet" "tfsubnet2" {
+    name                 = "LabSubnet2"
+    resource_group_name = var.ResourceGroup
+    virtual_network_name = azurerm_virtual_network.TFNet.name
+    address_prefix       = "10.0.2.0/24"
+}
--- exercise01/variable.tf
+++ exercise01/variable.tf
@@ -3,11 +3,11 @@ variable "region"{
     description="Azure Region"
 }
 variable "ResourceGroup"{
-    default="156-ddfdcfc1-deploy-an-azure-storage-account-with"
+    default="155-5e763b6d-deploy-azure-vlans-and-subnets-with-t"
     description="Here goes the resource group for the storage account. copy the default from the play ground"
 }
 
 variable "Storage_Account_name"{
-    default="accountfortoday8172021"
+    default="accountfortoday8182021"
     description="Here goes the resource group for the storage account, it needs to be unique, so copy with the date"
 }
