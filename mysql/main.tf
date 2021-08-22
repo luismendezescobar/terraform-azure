99e12ffb4901b43759ed2ef2b837fb26f4594e3f
esf25t3 <luis.mendez@atos.net>
2021-08-22 13:06:09 -0500

    adding changes of today 8222021-13

--- mysql/main.tf
+++ mysql/main.tf
@@ -27,24 +27,22 @@ resource "azurerm_mysql_server" "example" {
   name                = "mysql-8222021"
   location            = var.region
   resource_group_name = var.ResourceGroup
-
-  sku_name {
-    name     = "B_Gen5_2"
-    capacity = 2
-    tier     = "Basic"
-    family   = "Gen5"
-  }
-
-  storage_profile {
-    storage_mb            = 5120
-    backup_retention_days = 7
-    geo_redundant_backup  = "Disabled"
-  }
-
+  
   administrator_login          = "mysqladminun"
   administrator_login_password = "Passw0rd!"
-  version                      = "5.7"
-  ssl_enforcement              = "Enabled"
+
+  sku_name = "B_Gen5_2"
+  storage_mb = 5120
+  version="5.7"
+  
+  auto_grow_enabled                 = true
+  backup_retention_days             = 7
+  geo_redundant_backup_enabled      = false
+  infrastructure_encryption_enabled = false
+  public_network_access_enabled     = true
+  ssl_enforcement_enabled           = true
+  ssl_minimal_tls_version_enforced  = "TLS1_2"
+  
 }
 
 resource "azurerm_mysql_database" "example" {
